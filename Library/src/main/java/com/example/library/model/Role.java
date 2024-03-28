f0446f80c2a6ea0903e6d924295a31f5e08a52a5
kitou88C39 <kanfutrooper@icloud.com>
2024-03-28 20:15:56 +0900

    Library/src/main/java/com.ecommerce.library/modelにRole.javaを作成する

--- Library/src/main/java/com/example/library/model/Role.java
+++ Library/src/main/java/com/example/library/model/Role.java
@@ -1,8 +1,6 @@
 package com.example.library.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -12,6 +10,7 @@ import lombok.NoArgsConstructor;
 @Table(name = "roles")
 public class Role {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     private  String name;
