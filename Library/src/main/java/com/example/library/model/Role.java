4f39f9d91d8497ced8920732d01d11d955089dc5
kitou88C39 <kanfutrooper@icloud.com>
2024-03-28 20:09:27 +0900

    Library/src/main/java/com.ecommerce.library/modelにRole.javaを作成する

--- Library/src/main/java/com/example/library/model/Role.java
+++ Library/src/main/java/com/example/library/model/Role.java
@@ -1,10 +1,12 @@
-package com.ecommerce.library.model;
+package com.example.library.model;
 
+import jakarta.persistence.Entity;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data @NoArgsConstructor @AllArgsConstructor
+@Entity
 public class Role {
     private Long id;
 
