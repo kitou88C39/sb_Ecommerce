7997aa0ab29b773be4f9f876d4f32e0d7208fc5d
kitou88C39 <kanfutrooper@icloud.com>
2024-03-29 17:52:15 +0900

    Library/src/main/java/com.ecommerce.library/repositoryにAdminRepository.javaを作成する

--- Library/src/main/java/com/example/library/repository/AdminRepository.java
+++ Library/src/main/java/com/example/library/repository/AdminRepository.java
@@ -2,9 +2,12 @@ package com.example.library.repository;
 
 import com.example.library.model.Admin;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface AdminRepository extends JpaRepository<Admin, Long> {
+
+    @Query(value = "query", nativeQuery = true)
     Admin findByUsername(String username);
 }
