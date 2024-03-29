6e8370f3f85fa869f807ae410455c13cec39724f
kitou88C39 <kanfutrooper@icloud.com>
2024-03-29 20:28:48 +0900

    Library/src/main/java/com.ecommerce.library/serviceにimplファイルAdminServiceImpl.javaを作成する

--- Library/src/main/java/com/example/library/service/imple/AdminServiceImpl.java
+++ Library/src/main/java/com/example/library/service/imple/AdminServiceImpl.java
@@ -1,2 +1,11 @@
-package com.example.library.service.imple;public class AdminServiceImpl {
+package com.example.library.service.imple;
+
+import com.example.library.dto.AdminDto;
+import com.example.library.model.Admin;
+import com.example.library.service.AdminService;
+public class AdminServiceImpl implements AdminService {
+    @Override
+    public Admin findByUsername(String username){
+        return null;
+    }
 }
