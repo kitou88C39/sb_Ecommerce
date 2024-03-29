360c545435dacbfd170884a0b5f687df60555e68
kitou88C39 <kanfutrooper@icloud.com>
2024-03-29 19:58:54 +0900

    Library/src/main/java/com.ecommerce.library/dtoにAdminDto.javaを作成する

--- Library/src/main/java/com/example/library/dto/AdminDto.java
+++ Library/src/main/java/com/example/library/dto/AdminDto.java
@@ -1,4 +1,10 @@
 package com.example.library.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data @NoArgsConstructor @AllArgsConstructor
 public class AdminDto {
     private String firstName;
 
@@ -7,4 +13,6 @@ public class AdminDto {
     private String username;
 
     private String password;
+
+    private String repeatPassword;
 }
