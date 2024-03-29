185b2e07f7d477d57a2567f32c271d8071a293a9
kitou88C39 <kanfutrooper@icloud.com>
2024-03-29 16:30:49 +0900

    Library/src/main/java/com.ecommerce.library/modelにAdmin.javaを作成する

--- Library/src/main/java/com/example/library/model/Admin.java
+++ Library/src/main/java/com/example/library/model/Admin.java
@@ -24,6 +24,7 @@ public class Admin {
     private  String image;
 
     @ManyToMany(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
-    @JoinTable(name = "admins_roles", joinColumns = @JoinColumn(name = "admin_id", referencedColumnName = "admin_id"))
+    @JoinTable(name = "admins_roles", joinColumns = @JoinColumn(name = "admin_id", referencedColumnName = "admin_id"),
+    inverseJoinColumns = @JoinColumn(name = "role_id", referencedColumnName = "role_id"))
     private Collection<Role> roles;
 }
