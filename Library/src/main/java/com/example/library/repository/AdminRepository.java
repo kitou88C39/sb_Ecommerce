c9c85b36394a8430c3fa8015ab26ffff6da3997c
kitou88C39 <kanfutrooper@icloud.com>
2024-03-29 17:19:06 +0900

    Library/src/main/java/com.ecommerce.library/repositoryにAdminRepository.javaを作成する

--- Library/src/main/java/com/example/library/repository/AdminRepository.java
+++ Library/src/main/java/com/example/library/repository/AdminRepository.java
@@ -1,2 +1,6 @@
-package com.example.library.repository;public interface AdminRepository {
+package com.example.library.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AdminRepository extends JpaRepository {
 }
