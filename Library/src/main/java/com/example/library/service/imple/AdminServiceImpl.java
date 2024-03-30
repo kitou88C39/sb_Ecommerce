9385f275150824ae0922954ec45237d99938973a
kitou88C39 <kanfutrooper@icloud.com>
2024-03-30 11:21:45 +0900

    Library/src/main/java/com.ecommerce.library/serviceにimplファイルAdminServiceImpl.javaを作成する

--- Library/src/main/java/com/example/library/service/imple/AdminServiceImpl.java
+++ Library/src/main/java/com/example/library/service/imple/AdminServiceImpl.java
@@ -2,6 +2,8 @@ package com.example.library.service.imple;
 
 import com.example.library.dto.AdminDto;
 import com.example.library.model.Admin;
+import com.example.library.repository.AdminRepository;
+import com.example.library.repository.RoleRepository;
 import com.example.library.service.AdminService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -10,12 +12,21 @@ import org.springframework.stereotype.Service;
 public class AdminServiceImpl implements AdminService {
 
     @Autowired
+    private AdminRepository adminRepository;
+
+    @Autowired
+    private RoleRepository roleRepository;
+
     @Override
     public Admin findByUsername(String username){
-        return null;
+        return adminRepository.findByUsername(username);
     }
     @Override
     public Admin save(AdminDto adminDto){
+        Admin admin = new Admin();
+        admin.setFirstName(adminDto.getFirstName());
+        admin.setLastName(adminDto.getLastName());
+        admin.setUsername(adminDto.getUsername());
         return null;
     }
 }
