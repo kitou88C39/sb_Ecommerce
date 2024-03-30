f15d48e43c8a93d90491175be077b1d51e92f95d
kitou88C39 <kanfutrooper@icloud.com>
2024-03-30 11:44:05 +0900

    Admin/src/java/com.example.admin/AdimnApplicationをLibraryを受け取るように修正する

--- Admin/src/main/java/com/example/admin/Admin/AdminApplication.java
+++ Admin/src/main/java/com/example/admin/Admin/AdminApplication.java
@@ -3,7 +3,7 @@ package com.example.admin.Admin;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "com.example.library.*")
 public class AdminApplication {
 
 	public static void main(String[] args) {
