68c4a3dba656fce232a78de3e4889810eeb4a930
kitou88C39 <kanfutrooper@icloud.com>
2024-03-31 11:31:42 +0900

    Admin/src/java/com.example.adminにconfigフォルダを作成し、AdminDetails.javaを作成する

--- Admin/src/main/java/com/example/admin/config/AdminDetails.java
+++ Admin/src/main/java/com/example/admin/config/AdminDetails.java
@@ -1,6 +1,20 @@
 package com.example.admin.config;
 
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
+import java.util.Collection;
+
 public class AdminDetails implements UserDetails {
+
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
+
+    @Override
+    public String getPassword() {
+        return null;
+    }
 }
