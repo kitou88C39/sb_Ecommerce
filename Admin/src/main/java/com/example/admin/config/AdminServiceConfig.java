1be6ffac4b416e1e6a5498f8c86d9863590111ac
kitou88C39 <kanfutrooper@icloud.com>
2024-03-31 13:40:37 +0900

    Admin/src/java/com.example.admin/configにAdminServiceConfig.javaを作成する

--- Admin/src/main/java/com/example/admin/config/AdminServiceConfig.java
+++ Admin/src/main/java/com/example/admin/config/AdminServiceConfig.java
@@ -1,10 +1,14 @@
 package com.example.admin.config;
 
+import com.example.library.repository.AdminRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 
 public class AdminServiceConfig implements UserDetailsService {
+    @Autowired
+    private AdminRepository adminRepository;
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         return null;
