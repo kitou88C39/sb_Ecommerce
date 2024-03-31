f65de53a946ba8494724fba02483c38722028503
kitou88C39 <kanfutrooper@icloud.com>
2024-03-31 11:42:45 +0900

    Admin/src/java/com.example.adminにconfigフォルダを作成し、AdminDetails.javaを作成する

--- Admin/src/main/java/com/example/admin/config/AdminDetails.java
+++ Admin/src/main/java/com/example/admin/config/AdminDetails.java
@@ -15,31 +15,31 @@ public class AdminDetails implements UserDetails {
 
     @Override
     public String getPassword() {
-        return null;
+        return admin.getPassword();
     }
 
     @Override
     public String getUsername() {
-        return null;
+        return admin.getUsername();
     }
 
     @Override
     public boolean isAccountNonExpired() {
-        return false;
+        return true;
     }
 
     @Override
     public boolean isAccountNonLocked() {
-        return false;
+        return true;
     }
 
     @Override
     public boolean isCredentialsNonExpired() {
-        return false;
+        return true;
     }
 
     @Override
     public boolean isEnabled() {
-        return false;
+        return true;
     }
 }
