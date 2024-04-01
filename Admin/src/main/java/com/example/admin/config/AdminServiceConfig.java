292741ca61231a57e73e4a684b896d088fed3e63
kitou88C39 <kanfutrooper@icloud.com>
2024-04-01 15:57:29 +0900

    Admin/src/java/com.example.admin/configにAdminServiceConfig.javaを作成する

--- Admin/src/main/java/com/example/admin/config/AdminServiceConfig.java
+++ Admin/src/main/java/com/example/admin/config/AdminServiceConfig.java
@@ -3,9 +3,12 @@ package com.example.admin.config;
 import com.example.library.model.Admin;
 import com.example.library.repository.AdminRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+import java.util.stream.Collectors;
 
 public class AdminServiceConfig implements UserDetailsService {
     @Autowired
@@ -16,6 +19,11 @@ public class AdminServiceConfig implements UserDetailsService {
         if(admin == null){
             throw new UsernameNotFoundException("Could not find username");
         }
-        return null;
+        return new User(
+                admin.getUsername(),
+                admin.getPassword(),
+                admin.getRoles().stream().map(mapper.roke -> new SimpleGrantedAuthority(role.getName())).collect(Collectors.toList());
+
+
     }
 }
