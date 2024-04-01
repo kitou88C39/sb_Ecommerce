b71b937b619bd03029365225b0ecedc1743500bc
kitou88C39 <kanfutrooper@icloud.com>
2024-04-01 16:34:07 +0900

    Admin/src/java/com.example.admin/configにAdminConfiguration.javaを作成する

--- Admin/src/main/java/com/example/admin/config/AdminConfiguration.java
+++ Admin/src/main/java/com/example/admin/config/AdminConfiguration.java
@@ -3,7 +3,9 @@ package com.example.admin.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
@@ -13,4 +15,7 @@ public class AdminConfiguration extends WebSecurityConfigurerAdapter {
     public UserDetailsService userDetailsService(){
         return new AdminServiceConfig();
     }
+
+    @Bean
+    public BCryptPasswordEncoder
 }
