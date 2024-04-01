9dbb19b47a400654b861fbc07388077a193dcc91
kitou88C39 <kanfutrooper@icloud.com>
2024-04-01 16:24:33 +0900

    Admin/src/java/com.example.admin/configにAdminConfiguration.javaを作成する

--- Admin/src/main/java/com/example/admin/config/AdminConfiguration.java
+++ Admin/src/main/java/com/example/admin/config/AdminConfiguration.java
@@ -1,4 +1,9 @@
 package com.example.admin.config;
 
-public class AdminConfiguration {
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+@Configuration
+@EnableWebSecurity
+public class AdminConfiguration extends WebSecurityConfigurerAdapter {
 }
