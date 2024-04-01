be3d9afbffaf44b3f55a7682704223024d28da5a
kitou88C39 <kanfutrooper@icloud.com>
2024-04-01 21:34:20 +0900

    Admin/src/java/com.example.adminにcontrollerフォルダを作成し、LoginController.javaを作成する

--- Admin/src/main/java/com/example/admin/controller/LoginController.java
+++ Admin/src/main/java/com/example/admin/controller/LoginController.java
@@ -1,7 +1,13 @@
 package com.example.admin.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class LoginController {
+
+    @GetMapping("/login")
+    public String loginForm(){
+        return "login";
+    }
 }
