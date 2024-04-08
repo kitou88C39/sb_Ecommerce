0af2a75069d6acae86e2431c30da1bdc3f53b336
kitoumasaomi <kanfutrooper@gmail.com>
2024-04-08 20:49:12 +0900

    Library/src/main/java/com.ecommerce.library/modelにCategory.javaを作成する

--- Library/src/main/java/com/example/library/model/Category.java
+++ Library/src/main/java/com/example/library/model/Category.java
@@ -1,5 +1,11 @@
 package com.example.library.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+
 public class Category {
     private Long id;
     private String name;
