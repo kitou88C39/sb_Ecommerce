b2a4dbc8c43c22c7b2e0e57de437c41c8109cf1b
kitoumasaomi <kanfutrooper@gmail.com>
2024-04-08 20:49:12 +0900

    Library/src/main/java/com.ecommerce.library/modelにCategory.javaを作成する

--- Library/src/main/java/com/example/library/model/Category.java
+++ Library/src/main/java/com/example/library/model/Category.java
@@ -1,5 +1,16 @@
 package com.example.library.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import  javax.persistence.*;
+
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = categories, uniqueConstraints = @UniqueConstraint(columnNames="name"))
 public class Category {
     private Long id;
     private String name;
