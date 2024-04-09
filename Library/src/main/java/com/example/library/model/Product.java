0a1b0f4f8de38346b04ea991a20e02a7d06e6059
kitoumasaomi <kanfutrooper@gmail.com>
2024-04-09 11:35:53 +0900

    Library/src/main/java/com.ecommerce.library/modelにCategory.javaを作成する

--- Library/src/main/java/com/example/library/model/Product.java
+++ Library/src/main/java/com/example/library/model/Product.java
@@ -1,4 +1,10 @@
 package com.example.library.model;
 
 public class Product {
+    private Long id;
+    private String name;
+    private String description;
+    private double constPrice;
+    private double salePrice;
+    private int currentQuantity;
 }
