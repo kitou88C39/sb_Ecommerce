16254d9750882e25fcf63f04c75be1c14941ff22
kitoumasaomi <kanfutrooper@gmail.com>
2024-04-09 20:43:55 +0900

    Library/src/main/java/com.ecommerce.library/modelにProduct.javaを作成する

--- Library/src/main/java/com/example/library/repository/ProductRepository.java
+++ Library/src/main/java/com/example/library/repository/ProductRepository.java
@@ -1,4 +1,7 @@
 package com.example.library.repository;
 
+import com.example.library.model.Product;
+import org.springfreamwork.data.jpa.Repository.JpaRepository;
+import org.springfreamwork.stereotype.Repository;
 public interface ProductRepository extends JpaRepository<Product, Long>{
 }
