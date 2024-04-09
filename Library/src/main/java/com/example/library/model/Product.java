03361a1206c72e0715c18581c50629913a2a7326
kitoumasaomi <kanfutrooper@gmail.com>
2024-04-09 15:27:47 +0900

    Library/src/main/java/com.ecommerce.library/modelにProduct.javaを作成する

--- Library/src/main/java/com/example/library/model/Product.java
+++ Library/src/main/java/com/example/library/model/Product.java
@@ -1,14 +1,23 @@
 package com.example.library.model;
 
-import jdk.jfr.Category;
-
+import javax.persistence.*
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "products", uniqueConstraints = @UniqueConstraint(columnNames= {"name","image"}))
 public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.INDENITY)
+    @Column(name = "product_id")
     private Long id;
     private String name;
     private String description;
     private double constPrice;
     private double salePrice;
     private int currentQuantity;
+    @Lob
+    @Column(columnDefinition = "MEDIUMBLOB")
     private String image;
     private Category category;
     private boolean is_deleted;
