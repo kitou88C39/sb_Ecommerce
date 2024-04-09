f17695987950050014455136297d2c37ab4f3220
kitoumasaomi <kanfutrooper@gmail.com>
2024-04-09 15:27:47 +0900

    Library/src/main/java/com.ecommerce.library/modelにProduct.javaを作成する

--- Library/src/main/java/com/example/library/model/Category.java
+++ Library/src/main/java/com/example/library/model/Category.java
@@ -3,7 +3,7 @@ package com.example.library.model;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import  javax.persistence.*;
+import javax.persistence.*;
 
 
 @Data
