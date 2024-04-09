b96fd1c1af86470f374dbe219f89d2313280f1e1
kitoumasaomi <kanfutrooper@gmail.com>
2024-04-09 20:43:55 +0900

    Library/src/main/java/com.ecommerce.library/modelにProduct.javaを作成する

--- Library/src/main/java/com/example/library/repository/CategoryRepository.java
+++ Library/src/main/java/com/example/library/repository/CategoryRepository.java
@@ -1,6 +1,7 @@
 package com.example.library.repository;
 
 import com.example.library.model.Category;
+import org.springfreamwork.data.jpa.Repository.JpaRepository;
 import org.springfreamwork.stereotype.Repository;
 
 @Repository
