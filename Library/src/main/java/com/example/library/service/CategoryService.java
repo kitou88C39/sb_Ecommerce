440353ceb626c7c5894b8c4d14d0cb6a35de0855
kitoumasaomi <kanfutrooper@gmail.com>
2024-04-13 10:37:34 +0900

    Library/src/main/java/com.ecommerce.library/serviceにCategoryServiceに作成する

--- Library/src/main/java/com/example/library/service/CategoryService.java
+++ Library/src/main/java/com/example/library/service/CategoryService.java
@@ -1,4 +1,11 @@
 package com.example.library.service;
 
+import com.example.library.Category;
+import java.util.List;
+
 public interface CategoryService {
+    List<Category> findAll();
+    Category save(Category category);
+    Category getById(Long id);
+    Category update(Category category);
 }
